52c4fedaafbcafdd39c801214ce834917adb3a21
Ayush Singh <ayushsingh4167@gmail.com>
2026-09-09 14:10:56 +0530

    Add html pages and link extractor structure

---
+++ src/searchengine/crawler/LinkExtractor.java
@@ -0,0 +1,13 @@
+package searchengine.crawler;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Set;
+
+public class LinkExtractor {
+
+    public Set<String> extractLinks(Path htmlFile) throws IOException {
+        // TODO
+        return null;
+    }
+}
